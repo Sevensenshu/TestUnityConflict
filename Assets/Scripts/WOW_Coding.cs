e7592d9f6b7a8b9cf5a7dafcd42e3c88035f15d4
Fabien <fabienpiffet73@hotmail.fr>
2025-11-07 16:06:49 +0100

    edit script value

--- Assets/Scripts/WOW_Coding.cs
+++ Assets/Scripts/WOW_Coding.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class WOW_Coding : MonoBehaviour
 {
+    private int jpp = 69;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
